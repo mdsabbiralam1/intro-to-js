a283e4931bd40846fc23e50e02b856b2544b559a
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-01 22:59:41 +0600

    added all code

--- variable-name.js
+++ variable-name.js
@@ -9,4 +9,22 @@ var delete = "sabbir"
 var nayok name = "hero alom"
 
 // quite is note allow
-var "nayika" = "shabnur"
+var "nayika" = "shabnur";
+
+
+var my-official - address = "andorkillah bandorban";
+var my_official_address = "andorkillah bandorban";
+var myofficialaddress = "andorkillah bandorban";
+var MyOfficialAddress = "andorkillah bandorban";
+
+var PIE = 3.14;
+
+
+var 99price = 256;
+var price99 = 545;
+var p544da = "sadf";
+
+
+
+var userHomeAddress = "katabon"
+var uha = "katabon";
