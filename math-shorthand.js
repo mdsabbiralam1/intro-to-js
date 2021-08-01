8564cbc4bfa867d9c07cd6cb77ce2856329201b0
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-02 01:08:41 +0600

    added short hand file

---
+++ math-shorthand.js
@@ -0,0 +1,25 @@
+// recap
+var price1 = 50;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price1 - price2;
+var multiplication = price1 * price2;
+var division = price1 / price2
+
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+var doublePrice = price2 * 2;
+// var newPrice = price1 + 10;
+// price1 = price1 + 10;
+// price1 += 10
+price1++
+console.log(doublePrice);
+// console.log(newPrice);
+console.log(price1);
+
+
+var love = 100;
+love--;
+console.log(love);
