ba6ef289b80eea896f9084ee1ad54e445c44581f
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-01 22:45:21 +0600

    added file

---
+++ first.js
@@ -0,0 +1,13 @@
+console.log(54);
+var price = 21;
+var age = 19;
+var tempareture = 31;
+var book = 300;
+var internet = 23;
+var name = "sabbir alam";
+var address = "tangail dhake"
+
+
+var collager = "BAF shaheen collage and paharchnchonpur"
+
+var pass = true"
---
+++ variable-name.js
@@ -0,0 +1,12 @@
+var name = "hero alom"
+
+
+// resarved keyword not allow
+var false = "23"
+var delete = "sabbir"
+
+// name should be one word
+var nayok name = "hero alom"
+
+// quite is note allow
+var "nayika" = "shabnur"
