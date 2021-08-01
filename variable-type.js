036e4af8e501ca353efccc93c4f38a23e0d53c5b
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-02 02:56:45 +0600

    add variable type

---
+++ variable-type.js
@@ -0,0 +1,18 @@
+var botolPrice = 500;
+console.log(typeof botolPrice);
+var botolPrice1 = "500";
+console.log(typeof botolPrice1);
+
+var name = "sabbir";
+console.log(typeof name);
+
+
+var today = new Date()
+console.log(typeof today);
+
+var pc = 0.2
+var mouse = 0.1
+var pcPrice = pc + mouse;
+pcPrice = pcPrice.toFixed(5);
+pcPrice = parsefloat(pcPrice);
+console.log(pcPrice);
