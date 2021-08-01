3ff5ceab71ace08aff41b2c9e1f13dfe42ac11b9
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-02 03:38:26 +0600

    added sumary file

---
+++ summary.js
@@ -0,0 +1,21 @@
+// number
+var penPrice = 30;
+console.log(penPrice);
+
+// string
+var bookPrice = '40';
+console.log(bookPrice);
+
+// boolean
+var friday = true;
+console.log(friday);
+
+// date
+var today = new Date();
+console.log(today);
+
+var jh = 17;
+var kk = 5;
+var to = jh % kk;
+
+console.log(to);
