a111eb05bcafdecc80e23cff60659f271afda6de
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-02 00:17:50 +0600

    add +-*/

---
+++ mathematical-oparetion.js
@@ -0,0 +1,28 @@
+var eggprice = 10;
+var onionprice = 45;
+var eggQuantity = 7;
+
+
+var eggPriceMultiplication = eggprice * eggQuantity;
+var totalPrice = eggprice + onionprice;
+var priceDifference = eggprice - onionprice;
+
+
+// console.log(eggprice);
+// console.log(onionprice);
+// console.log(eggprice + onionprice);
+// console.log(eggprice - onionprice);
+// console.log(eggprice * onionprice);
+
+// console.log(totalPrice);
+// console.log(totalPrice + onionprice);
+// console.log(priceDifference);
+// console.log(eggPriceMultiplication);
+
+var moneyAvilable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvilable / orangePrice;
+
+
+console.log(orangeQuantity);
+
