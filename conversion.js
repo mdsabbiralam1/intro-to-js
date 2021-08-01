d66430f7d3290c0dd2ec561c9c3dbb1976ef63cd
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-02 02:17:26 +0600

    added conversion file

---
+++ conversion.js
@@ -0,0 +1,28 @@
+var firstName = 'Md Sabbir';
+var lastName = 'Alam';
+var fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+var first = 'developer';
+var last = 'jahed';
+var name = first + ' ' + last;
+// console.log(name);
+
+
+// var onionPrice = '44';
+// var eggPrice = '40';
+// var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+// integer digit
+var price = 545;
+// float digit
+var price = 3.6;
+
+var onionPrice = '20.5';
+var eggPrice = '54.5';
+var onionNumberPrice = parseInt(onionPrice);
+var eggNumberPrice = parsefloat(eggPrice);
+console.log(onionNumberPrice + eggNumberPrice);
+// var eggNumberPrice = preseInt(eggPrice);
